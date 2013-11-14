f32b4e67a6940431b31025ff44343cef6b24c82f
Santos Santos <ssantos@doubleleft.com>
2013-11-14 11:25:37 -0200

    Primeira versão

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+
+		stylus: {
+
+			compile: {
+
+				files: {
+					'css/main.min.css': ['sources/css/*.styl']
+				},
+
+				options: {
+					compress: true
+				}
+
+			}
+
+		},
+
+		uglify: {
+			compress: {
+				files: {
+					'js/main.min.js': ['sources/js/**/*.js', 'sources/js/*.js']
+				},
+				options: {
+					mangle: false
+				}
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['sources/js/**/*.js', 'sources/js/*.js'],
+				tasks: ['uglify'],
+				options: {
+					spawn: false
+				}
+			},
+			css: {
+				files: ['sources/css/*.styl','sources/css/**/*.styl'],
+				tasks: ['stylus'],
+				options: {
+					spawn: false
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['uglify', 'stylus']);
+};
